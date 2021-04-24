6b0660a52f697a4a4f4624d4edd88d1802c278f8
NNN38 <72020406+NNN38@users.noreply.github.com>
2021-04-24 22:13:51 +0200

    Add files via upload

---
+++ P2W Alertbox.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         P2W Alertbox
+// @match        https://p2w.pl/profile/38175-minecraftazuss/
+// @match        https://p2w.pl/profile/38175-minecraftazuss
+// @match        https://www.p2w.pl/profile/38175-minecraftazuss/
+// @match        https://www.p2w.pl/profile/38175-minecraftazuss/
+// @version      0.1
+// @author       nuio00
+// ==/UserScript==
+
+alert("O To azo, największy idiota serwera")
+
